1e8a1862d01771afff8bf91ffacea6f47ab469f6
jay-newman <97626791+jay-newman@users.noreply.github.com>
2022-12-02 15:27:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -18,44 +18,34 @@ client.on('messageCreate', message => {
     if (message.content === 'ping') {
         message.reply('pong');
     }
-
-    if (message.content === 'ricky') {
-        
+    if (message.content === 'ricky') {     
         message.reply({ files: [{ attachment: 'ricky.jpg' }] }); 
     }
-    
     if (message.content === 'lahey') {
         message.reply({ files: [{ attachment: 'jim.jpg'}]});
     }
-
     if (message.content === 'jim') {
         message.reply({ files: [{ attachment: 'lahey.gif'}]});
     }
-
     if (message.content === 'sweet') {
         message.reply({ files: [{ attachment: 'ricky2.gif'}]});
     }
-
     if (message.content === 'drink') {
         message.reply({ files: [{ attachment: 'drunkjim.gif'}]});
     }
-
     if (message.content === 'knock knock') {
         message.reply({ files: [{ attachment: 'knockknock.gif'}]});
     }
-
     if (message.content === 'fran') {
         message.reply({ files: [{ attachment: 'farmerfran.gif'}]});
     }
     if (message.content === 'hand') {
         message.reply({ files: [{ attachment: 'stronghand.gif'}]});
     }
-
     if (message.content === 'fav') {
         message.reply({ files: [{ attachment: 'laugh.gif'}]});
     }
 });
-
 client.login(process.env.TOKEN);
 
 
